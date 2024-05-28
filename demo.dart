24bf1ddc7bda72f026b556c160e1e35dcf19a242
Rahul Modhave <rahulmodhave099@gmail.com>
2024-05-28 17:35:01 +0530

    First Dart Code

--- demo.dart
+++ demo.dart
@@ -1,12 +1,16 @@
-import 'dart:ffi';
 
 void main()
 {
-  print("Hello World");
+  // This is my first code
+  print("Hello World");             // Another comment
   print("This is my First Application");
-
+  // Printing numerical value
   print(202);
-
-  print(4 * 2)
+  /*
+  Multiple lines comment.
+  printing boolean value 
+  */
+  print(true);
+  print(4 * 2);
 
 }
