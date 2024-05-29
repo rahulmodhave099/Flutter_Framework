18695f7aa99665c7c5091691874106aaf94ece84
Rahul Modhave <rahulmodhave099@gmail.com>
2024-05-29 19:00:07 +0530

    final and const variables

---
+++ final_&_const_variable.dart
@@ -0,0 +1,18 @@
+void main()
+{
+  // final keyword 
+  final cityName = "Pune";
+
+  final String countryName = 'India';
+
+  // const variable
+
+  const PI = 3.14;
+  const double gravity = 9.8;
+
+}
+
+class Circle{
+  final color = "Red";
+  static const PI = 3.14;
+}
