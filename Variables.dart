21685ef2c3ac70e7559ec61523dd443659a4e88c
Rahul Modhave <rahulmodhave099@gmail.com>
2024-05-29 18:58:13 +0530

    Variables Declaration

---
+++ Variables.dart
@@ -0,0 +1,41 @@
+
+void main()
+{
+  // Numbers : int
+  int age = 23;
+  var count = 22;             // it is inferred as integer automatically by compiler
+
+  int hexadecimal = 0xEADFDEAA;
+
+  // Numbers : double
+
+  double pecentage = 75.54;
+  var percent = 82.4445;
+  double exponent = 1.42e5; 
+
+  // String 
+
+  String name = "Rahul";
+  var comp = "Core2web";
+
+  // boolean
+
+  bool isValid = true;
+  var isAlive = true;
+
+  print(age);
+  print(count);
+  print(hexadecimal);
+
+  print(pecentage);
+  print(percent);
+  print(exponent);
+
+  print(name);
+  print(comp);
+
+  print(isValid);
+  print(isAlive);
+
+
+}
