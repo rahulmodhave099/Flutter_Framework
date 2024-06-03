c4f44bd368008efd3e303cf7e48944be11cbdee8
Rahul Modhave <rahulmodhave099@gmail.com>
2024-06-03 19:08:51 +0530

    switch case conditional Statement

---
+++ For_loop.dart
@@ -0,0 +1,14 @@
+void main()
+{
+  for(int i=1 ; i<=10 ; i++){
+    if(i % 2 == 0){
+      print(i);
+    }
+  }
+
+  List india = ["Rohit","Virat","Surya","Shreyash","KLRahul"];
+
+  for (var player in india) {
+    print(player);
+  }
+}
