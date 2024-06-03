2f00291ac88069bec267c6ba40fb87a8afc28676
Rahul Modhave <rahulmodhave099@gmail.com>
2024-06-04 00:29:33 +0530

    for loop

--- For_loop.dart
+++ For_loop.dart
@@ -12,26 +12,5 @@ void main()
     print(player);
   }
 
-  // break statement
 
-  for (int i = 1 ; i <= 3 ; i++){
-    for (int j = 1 ; j <= 3 ; j++){
-      print("$i $j");
-      if(i == 2 && j == 2){
-        break;
-    }
-    }
-  }
-
-  // Using labels
-print("############################################################################");
-
-  myouterloop: for (int i = 1 ; i <= 3 ; i++){
-    myinnerloop: for (int j = 1 ; j <= 3 ; j++){
-      print("$i $j");
-      if(i == 2 && j == 2){
-        break myouterloop;
-    }
-    }
-  }
 }
