ea5dda3321e150973bc66bfe8cf73345ad7035ca
Rahul Modhave <rahulmodhave099@gmail.com>
2024-06-04 00:30:13 +0530

    break statement

---
+++ break_&_continue_statement.dart
@@ -0,0 +1,26 @@
+void main()
+{
+    // break statement
+
+  for (int i = 1 ; i <= 3 ; i++){
+    for (int j = 1 ; j <= 3 ; j++){
+      print("$i $j");
+      if(i == 2 && j == 2){
+        break;
+    }
+    }
+  }
+
+  // Using labels
+print("############################################################################");
+
+  myouterloop: for (int i = 1 ; i <= 3 ; i++){
+    myinnerloop: for (int j = 1 ; j <= 3 ; j++){
+      print("$i $j");
+      if(i == 2 && j == 2){
+        break myouterloop;
+    }
+    }
+  }
+  
+}
