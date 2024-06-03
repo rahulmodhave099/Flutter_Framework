571828793ae7cee97ab60d4df0572a8df3714f44
Rahul Modhave <rahulmodhave099@gmail.com>
2024-06-04 00:21:29 +0530

    break statement

--- For_loop.dart
+++ For_loop.dart
@@ -11,4 +11,27 @@ void main()
   for (var player in india) {
     print(player);
   }
+
+  // break statement
+
+  for (int i = 1 ; i <= 3 ; i++){
+    for (int j = 1 ; j <= 3 ; j++){
+      print("$i $j");
+      if(i == 2 && j == 2){
+        break;
+    }
+    }
+  }
+
+  // Using labels
+print("############################################################################");
+
+  myouterloop: for (int i = 1 ; i <= 3 ; i++){
+    myinnerloop: for (int j = 1 ; j <= 3 ; j++){
+      print("$i $j");
+      if(i == 2 && j == 2){
+        break myouterloop;
+    }
+    }
+  }
 }
