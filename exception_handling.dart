ea484ff25810131de013b11aaaf7aacbdd80314d
Rahul Modhave <rahulmodhave099@gmail.com>
2024-06-05 13:53:13 +0530

    exception handling catch clause stack trace

--- exception_handling.dart
+++ exception_handling.dart
@@ -2,7 +2,7 @@ void main()
 {
   // OBJECTIVES 
   // on clause
-  // on clause is used when we know what exception is we getting 
+  // on clause is used when we know what exception object is we getting 
   print(" on clause");
   try{
   int result = 12~/0;
@@ -13,11 +13,23 @@ void main()
 
   print("catch clause");
   // catch clause
-  // when you dont know the exception name use catch clause
-    try{
+  // when you dont know the exception object name use catch clause
+  try{
   int result = 12~/0;
   print(result);
   } catch(e) {
     print("The exception thrown is $e");
   }
+  print("catch clause stack trace");
+  // catch clause with exception object and StackTrace object
+  // Using StackTrace to know the events occured before exception was thrown
+
+  try{
+  int result = 12~/0;
+  print(result);
+  } catch(e, s) {
+    print("The exception thrown is $e");
+    print("Stack Trace \n $s");
+  }
+
 }
